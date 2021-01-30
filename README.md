c4d3fa6c5458e71ebb6cada15a4026fb7c964150
Martin Kim Dung-Pham <kim.dung-pham@xing.com>
2021-01-30 21:10:19 +0100

    Experiment with brightness

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 `cargo build`
 
 `avrdude -p t85 -c avrispmkII -U flash:w:target/avr-attiny85/debug/avr-attiny85.elf`
+
+on      off
+10ms    10ms
+5ms     5ms
+1ms     20ms
+1ms     10ms
+1ms     5ms
+10ms    1ms
--- src/main.rs
+++ src/main.rs
@@ -14,30 +14,26 @@ use attiny85_hal::prelude::*;
 fn main() -> ! {
     let dp = attiny85_hal::pac::Peripherals::take().unwrap();
     let mut portb = dp.PORTB.split();
-    let mut led0: PB0<mode::Output> = portb.pb0.into_output(&mut portb.ddr);
-    let mut led9: PB1<mode::Output> = portb.pb1.into_output(&mut portb.ddr);
-    let mut x = 0u16;
+    let mut led0 = portb.pb0.into_output(&mut portb.ddr);
+    let mut _led9 = portb.pb1.into_output(&mut portb.ddr);
 
+    let mut x = 0u16;
     loop {
         x = x + 1;
-
-        led0.toggle().void_unwrap();
-        led9.toggle().void_unwrap();
-
         let mut delay = Delay::<MHz1>::new();
-        delay.delay_ms(x);
+
+        led0.set_high().void_unwrap();
+
+        delay.delay_ms(1u8);
+
+        led0.set_low().void_unwrap();
+
+        delay.delay_ms(20u8);
 
         if x == 500 {
             x = 0;
         }
-
-        // for x in 0u16..500u16 {}
     }
 }
 
-fn blink(led: &mut PB0<mode::Output>, x: u16) {
-    led.toggle().void_unwrap();
-
-    let mut delay = Delay::<MHz1>::new();
-    delay.delay_us(5000u16);
-}
+fn blink(led: &mut PB0<mode::Output>, x: u16) {}
