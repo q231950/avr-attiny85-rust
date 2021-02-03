44f06f379cc0ae42085e0c4c9b8dc66d3a6d5fec
Martin Kim Dung-Pham <kim.dung-pham@xing.com>
2021-02-03 21:21:02 +0100

    No flicker, butter smooth 8MHz

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
 
 # blink on attiny
 
+## Build
+
 `cargo build`
 
+## Flash
+
 `avrdude -p t85 -c avrispmkII -U flash:w:target/avr-attiny85/debug/avr-attiny85.elf`
+
+## Configure Speed
+
+No matter which speed you select, remember to also adjust the timer to respect the setting:
+
+```rust
+let mut delay = Delay::<MHz8>::new();
+```
+
+**Factory default 1MHz prescaler**
+
+`avrdude -p t85 -c avrispmkII -U lfuse:w:0x62:m`
+
+**8 MHz**
+
+`avrdude -p t85 -c avrispmkII -U lfuse:w:0xe2:m`
+
+## Chip Interrogation
+
+`-U lfuse:r:-:i`
+
+
 
 on      off
 10ms    10ms
--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,112 @@ use attiny85_hal::prelude::*;
 fn main() -> ! {
     let dp = attiny85_hal::pac::Peripherals::take().unwrap();
     let mut portb = dp.PORTB.split();
-    let mut led0 = portb.pb0.into_output(&mut portb.ddr);
-    let mut _led9 = portb.pb1.into_output(&mut portb.ddr);
 
-    let mut x = 0u16;
+    let mut a = portb.pb0.into_output(&mut portb.ddr);
+    let mut b = portb.pb1.into_output(&mut portb.ddr);
+    let mut c = portb.pb2.into_output(&mut portb.ddr);
+    let mut d = portb.pb3.into_output(&mut portb.ddr);
+    let mut led = portb.pb4.into_output(&mut portb.ddr);
+    let mut delay = Delay::<MHz8>::new();
+
+    // let mut animation_clock = 0u16;
+    let mut index = 0u8;
+
     loop {
-        x = x + 1;
-        let mut delay = Delay::<MHz1>::new();
-
-        led0.set_high().void_unwrap();
-
-        delay.delay_ms(1u8);
-
-        led0.set_low().void_unwrap();
-
-        delay.delay_ms(20u8);
-
-        if x == 500 {
-            x = 0;
+        if index > 9 {
+            index = 0;
         }
+
+        led.set_low().void_unwrap();
+
+        enable_led_at_index(index, &mut a, &mut b, &mut c, &mut d);
+
+        led.set_high().void_unwrap();
+        // update(&mut led, index, animation_clock);
+
+        delay.delay_ms(2u8);
+
+        // if animation_clock == 500 {
+        //     animation_clock = 0;
+        // }
+
+        index = index + 1;
+        // animation_clock = animation_clock + 1;
     }
 }
 
-fn blink(led: &mut PB0<mode::Output>, x: u16) {}
+// fn update(led: &mut PB4<mode::Output>, _index: u8, _animation_clock: u16) {
+//     led.set_high().void_unwrap();
+// }
+
+fn enable_led_at_index(
+    index: u8,
+    a: &mut PB0<mode::Output>,
+    b: &mut PB1<mode::Output>,
+    c: &mut PB2<mode::Output>,
+    d: &mut PB3<mode::Output>,
+) {
+    match index {
+        0 => {
+            a.set_low().void_unwrap();
+            b.set_low().void_unwrap();
+            c.set_low().void_unwrap();
+            d.set_low().void_unwrap();
+        }
+        1 => {
+            a.set_high().void_unwrap();
+            // b.set_low().void_unwrap();
+            // c.set_low().void_unwrap();
+            // d.set_low().void_unwrap();
+        }
+        2 => {
+            a.set_low().void_unwrap();
+            b.set_high().void_unwrap();
+            // c.set_low().void_unwrap();
+            // d.set_low().void_unwrap();
+        }
+        3 => {
+            a.set_high().void_unwrap();
+            // b.set_high().void_unwrap();
+            // c.set_low().void_unwrap();
+            // d.set_low().void_unwrap();
+        }
+        4 => {
+            a.set_low().void_unwrap();
+            b.set_low().void_unwrap();
+            c.set_high().void_unwrap();
+            // d.set_low().void_unwrap();
+        }
+        5 => {
+            a.set_high().void_unwrap();
+            // b.set_low().void_unwrap();
+            // c.set_high().void_unwrap();
+            // d.set_low().void_unwrap();
+        }
+        6 => {
+            a.set_low().void_unwrap();
+            b.set_high().void_unwrap();
+            // c.set_high().void_unwrap();
+            // d.set_low().void_unwrap();
+        }
+        7 => {
+            a.set_high().void_unwrap();
+            // b.set_high().void_unwrap();
+            // c.set_high().void_unwrap();
+            // d.set_low().void_unwrap();
+        }
+        8 => {
+            a.set_low().void_unwrap();
+            b.set_low().void_unwrap();
+            c.set_low().void_unwrap();
+            d.set_high().void_unwrap();
+        }
+        9 => {
+            a.set_high().void_unwrap();
+            // b.set_low().void_unwrap();
+            // c.set_low().void_unwrap();
+            d.set_high().void_unwrap();
+        }
+        _ => (),
+    }
+}
