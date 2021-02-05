5ed9d119ee802fb5499d471d15e63c583df63142
Martin Kim Dung-Pham <kim.dung-pham@xing.com>
2021-02-05 20:03:44 +0100

    Walk the stairs with additional hardware and less code (aka make more space for code by throwing hardware at it)

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ extern crate panic_halt;
 
 use attiny85_hal::clock::*;
 use attiny85_hal::delay::Delay;
-use attiny85_hal::port::portb::*;
 use attiny85_hal::port::*;
 use attiny85_hal::prelude::*;
 
@@ -15,111 +14,73 @@ fn main() -> ! {
     let dp = attiny85_hal::pac::Peripherals::take().unwrap();
     let mut portb = dp.PORTB.split();
 
-    let mut a = portb.pb0.into_output(&mut portb.ddr);
-    let mut b = portb.pb1.into_output(&mut portb.ddr);
-    let mut c = portb.pb2.into_output(&mut portb.ddr);
-    let mut d = portb.pb3.into_output(&mut portb.ddr);
+    let mut pulse = portb.pb0.into_output(&mut portb.ddr);
+    let mut reset = portb.pb1.into_output(&mut portb.ddr);
     let mut led = portb.pb4.into_output(&mut portb.ddr);
     let mut delay = Delay::<MHz8>::new();
+    let mut animation_clock = 0u16;
+    let mut on: bool;
 
-    // let mut animation_clock = 0u16;
-    let mut index = 0u8;
+    pulse.set_high().void_unwrap();
+    reset.set_high().void_unwrap();
+    reset.set_low().void_unwrap();
 
     loop {
-        if index > 9 {
-            index = 0;
+        match animation_clock % 10 {
+            0 => {
+                on = animation_clock <= 50;
+            }
+            1 => {
+                on = animation_clock >= 50 && animation_clock <= 100;
+            }
+            2 => {
+                on = animation_clock >= 100 && animation_clock <= 150;
+            }
+            3 => {
+                on = animation_clock >= 150 && animation_clock <= 200;
+            }
+            4 => {
+                on = animation_clock >= 200 && animation_clock <= 250;
+            }
+            5 => {
+                on = animation_clock >= 250 && animation_clock <= 300;
+            }
+            6 => {
+                on = animation_clock >= 300 && animation_clock <= 350;
+            }
+            7 => {
+                on = animation_clock >= 350 && animation_clock <= 400;
+            }
+            8 => {
+                on = animation_clock >= 400 && animation_clock <= 450;
+            }
+            9 => {
+                on = animation_clock >= 450 && animation_clock <= 500;
+            }
+            _ => {
+                on = false;
+            }
         }
 
-        led.set_low().void_unwrap();
-
-        enable_led_at_index(index, &mut a, &mut b, &mut c, &mut d);
-
-        led.set_high().void_unwrap();
-        // update(&mut led, index, animation_clock);
+        if on {
+            led.set_high().void_unwrap();
+        }
 
         delay.delay_ms(2u8);
 
-        // if animation_clock == 500 {
-        //     animation_clock = 0;
-        // }
+        led.set_low().void_unwrap();
 
-        index = index + 1;
-        // animation_clock = animation_clock + 1;
-    }
-}
+        if animation_clock % 10 == 9 {
+            reset.set_high().void_unwrap();
+            reset.set_low().void_unwrap();
+        } else {
+            pulse.set_low().void_unwrap();
+            pulse.set_high().void_unwrap();
+        }
 
-// fn update(led: &mut PB4<mode::Output>, _index: u8, _animation_clock: u16) {
-//     led.set_high().void_unwrap();
-// }
-
-fn enable_led_at_index(
-    index: u8,
-    a: &mut PB0<mode::Output>,
-    b: &mut PB1<mode::Output>,
-    c: &mut PB2<mode::Output>,
-    d: &mut PB3<mode::Output>,
-) {
-    match index {
-        0 => {
-            a.set_low().void_unwrap();
-            b.set_low().void_unwrap();
-            c.set_low().void_unwrap();
-            d.set_low().void_unwrap();
+        animation_clock += 1;
+        if animation_clock == 500 {
+            animation_clock = 0;
         }
-        1 => {
-            a.set_high().void_unwrap();
-            // b.set_low().void_unwrap();
-            // c.set_low().void_unwrap();
-            // d.set_low().void_unwrap();
-        }
-        2 => {
-            a.set_low().void_unwrap();
-            b.set_high().void_unwrap();
-            // c.set_low().void_unwrap();
-            // d.set_low().void_unwrap();
-        }
-        3 => {
-            a.set_high().void_unwrap();
-            // b.set_high().void_unwrap();
-            // c.set_low().void_unwrap();
-            // d.set_low().void_unwrap();
-        }
-        4 => {
-            a.set_low().void_unwrap();
-            b.set_low().void_unwrap();
-            c.set_high().void_unwrap();
-            // d.set_low().void_unwrap();
-        }
-        5 => {
-            a.set_high().void_unwrap();
-            // b.set_low().void_unwrap();
-            // c.set_high().void_unwrap();
-            // d.set_low().void_unwrap();
-        }
-        6 => {
-            a.set_low().void_unwrap();
-            b.set_high().void_unwrap();
-            // c.set_high().void_unwrap();
-            // d.set_low().void_unwrap();
-        }
-        7 => {
-            a.set_high().void_unwrap();
-            // b.set_high().void_unwrap();
-            // c.set_high().void_unwrap();
-            // d.set_low().void_unwrap();
-        }
-        8 => {
-            a.set_low().void_unwrap();
-            b.set_low().void_unwrap();
-            c.set_low().void_unwrap();
-            d.set_high().void_unwrap();
-        }
-        9 => {
-            a.set_high().void_unwrap();
-            // b.set_low().void_unwrap();
-            // c.set_low().void_unwrap();
-            d.set_high().void_unwrap();
-        }
-        _ => (),
     }
 }
