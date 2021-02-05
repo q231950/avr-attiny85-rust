3c285a686f7a3cee663dcd2662ac61da97560fc4
Martin Kim Dung-Pham <kim.dung-pham@xing.com>
2021-02-05 20:27:20 +0100

    Cleanup code

--- src/main.rs
+++ src/main.rs
@@ -19,50 +19,12 @@ fn main() -> ! {
     let mut led = portb.pb4.into_output(&mut portb.ddr);
     let mut delay = Delay::<MHz8>::new();
     let mut animation_clock = 0u16;
-    let mut on: bool;
-
     pulse.set_high().void_unwrap();
     reset.set_high().void_unwrap();
     reset.set_low().void_unwrap();
 
     loop {
-        match animation_clock % 10 {
-            0 => {
-                on = animation_clock <= 50;
-            }
-            1 => {
-                on = animation_clock >= 50 && animation_clock <= 100;
-            }
-            2 => {
-                on = animation_clock >= 100 && animation_clock <= 150;
-            }
-            3 => {
-                on = animation_clock >= 150 && animation_clock <= 200;
-            }
-            4 => {
-                on = animation_clock >= 200 && animation_clock <= 250;
-            }
-            5 => {
-                on = animation_clock >= 250 && animation_clock <= 300;
-            }
-            6 => {
-                on = animation_clock >= 300 && animation_clock <= 350;
-            }
-            7 => {
-                on = animation_clock >= 350 && animation_clock <= 400;
-            }
-            8 => {
-                on = animation_clock >= 400 && animation_clock <= 450;
-            }
-            9 => {
-                on = animation_clock >= 450 && animation_clock <= 500;
-            }
-            _ => {
-                on = false;
-            }
-        }
-
-        if on {
+        if enabled(animation_clock) {
             led.set_high().void_unwrap();
         }
 
@@ -81,6 +43,22 @@ fn main() -> ! {
         animation_clock += 1;
         if animation_clock == 500 {
             animation_clock = 0;
+        }
+    }
+
+    fn enabled(animation_clock: u16) -> bool {
+        match animation_clock % 10 {
+            0 => animation_clock <= 50,
+            1 => animation_clock >= 50 && animation_clock <= 100,
+            2 => animation_clock >= 100 && animation_clock <= 150,
+            3 => animation_clock >= 150 && animation_clock <= 200,
+            4 => animation_clock >= 200 && animation_clock <= 250,
+            5 => animation_clock >= 250 && animation_clock <= 300,
+            6 => animation_clock >= 300 && animation_clock <= 350,
+            7 => animation_clock >= 350 && animation_clock <= 400,
+            8 => animation_clock >= 400 && animation_clock <= 450,
+            9 => animation_clock >= 450 && animation_clock <= 500,
+            _ => false,
         }
     }
 }
